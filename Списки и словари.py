1f02992b8807c6a55fe136727751fae6e313f3a0
Whale-99 <168347567+Whale-99@users.noreply.github.com>
2024-04-29 02:13:52 +0300

    Add files via upload

---
+++ Списки и словари.py
@@ -0,0 +1,15 @@
+my_list = ["Яблоко", "Груша", "Личи", "Арбуз", "Киви", "Малина"]
+print(my_list)
+print(my_list[0] + " " + my_list[-1])
+print(my_list[2:5])
+my_list[2] = "Голубика"
+print(my_list)
+print(
+
+)
+my_dict = {"Словарь": "dictionary", "Ключ": "key", "Перевод": "translation"}
+print(my_dict)
+print(my_dict["Словарь"])
+my_dict["Словарь"] = "glossary"
+my_dict["Знание"] = "knowledge"
+print(my_dict)
